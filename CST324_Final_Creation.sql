d297f508aa21b9dc04625c2e8f22abcc9577f4c7
RobertMooers <robert.mooers@oit.edu>
2016-11-22 11:49:17 -0800

    Added Initial Project Files

---
+++ CST324_Final_Creation.sql
@@ -0,0 +1,105 @@
+--ToTheRescue!
+--Creation scrip
+--Description: Creates and deletes the tables of the revised ER diagram
+
+--Note: Feel free to edit any of the VARCHAR(50) to a reasonable amount of characters
+USE lake_sainthomason;
+
+DROP TABLE ProfileProgressHistory;
+DROP TABLE MiniGameMedia;
+DROP TABLE MiniGames;
+DROP TABLE GameCategories;
+DROP TABLE ProfileAnimals;
+DROP TABLE ProfileProgress;
+DROP TABLE Profiles;
+DROP TABLE Users;
+DROP TABLE Animals;
+DROP TABLE Nodes;
+DROP TABLE Maps;
+DROP TABLE Sounds;
+DROP TABLE Images;
+
+CREATE TABLE CST324_Lake_Robert.Images (
+	ImageID INT PRIMARY KEY IDENTITY
+	, ImageClass INT NOT NULL
+	, Images VARBINARY(max) NOT NULL
+	, ImageName VARCHAR(50) NOT NULL);
+
+CREATE TABLE CST324_Lake_Robert.Sounds (
+	SoundID INT PRIMARY KEY IDENTITY
+	, SoundClass INT NOT NULL
+	, Sound VARBINARY(max) NOT NULL
+	, SoundName VARCHAR(50) NOT NULL);
+
+CREATE TABLE CST324_Lake_Robert.Maps (
+	MapID INT PRIMARY KEY IDENTITY
+	, MapName VARCHAR(50) NOT NULL
+	, ImageID INT REFERENCES Images (ImageID) NOT NULL
+	, SoundID INT REFERENCES Sounds (SoundID) NOT NULL);
+
+CREATE TABLE CST324_Lake_Robert.Nodes (
+	MapID INT REFERENCES Maps (MapID) NOT NULL
+	, NodeID INT IDENTITY
+	, XCoordinate INT NOT NULL
+	, YCoordinate INT NOT NULL
+	, PRIMARY KEY (MapID, NodeID));
+
+CREATE TABLE CST324_Lake_Robert.Animals (
+	AnimalID INT PRIMARY KEY IDENTITY
+	, FunFact VARCHAR(250)
+	, Shiny BIT NOT NULL --1 means it is shiny
+	, SoundID INT REFERENCES Sounds (SoundID) NOT NULL
+	, ImageID INT REFERENCES Images (ImageID) NOT NULL);
+
+CREATE TABLE CST324_Lake_Robert.Users (
+	UserID INT PRIMARY KEY IDENTITY
+	, UserPassword NVARCHAR(50) NOT NULL
+	, Username NVARCHAR(50) unique NOT NULL); --NVARCHAR allows for unicode characters in other languages
+
+CREATE TABLE CST324_Lake_Robert.Profiles (
+	ProfileID INT PRIMARY KEY IDENTITY
+	, UserID INT REFERENCES Users (UserID) NOT NULL
+	, AvatarID INT REFERENCES Images (ImageID) NOT NULL
+	, ProfileName NVARCHAR(30) NOT NULL
+	, ToggleSound BIT NOT NULL DEFAULT 1 --0 = sound off, 1 = sound on, defualted to sound on
+	, ToggleMusic BIT NOT NULL DEFAULT 1
+	, MathDifficultyLevel INT NOT NULL DEFAULT 1 --default to the lowest difficulty level
+	, MathPerformanceStat FLOAT NOT NULL
+	, ReadingDifficultyLevel INT NOT NULL DEFAULT 1 --default to the lowest difficulty level
+	, ReadingPerformanceStat FLOAT NOT NULL
+	, SubjectFilter VARCHAR(50));
+
+CREATE TABLE CST324_Lake_Robert.ProfileProgress (
+	ProfileID INT PRIMARY KEY REFERENCES Profiles (ProfileID) NOT NULL
+	, CurrentMap INT REFERENCES Maps (MapID) NOT NULL
+	, CurrentNode INT NOT NULL --current node number in the map
+	, AnimalID INT REFERENCES Animals (AnimalID) NOT NULL);
+
+CREATE TABLE CST324_Lake_Robert.ProfileAnimals (
+	ProfileAnimalID INT PRIMARY KEY IDENTITY
+	, AnimalID INT REFERENCES Animals (AnimalID) NOT NULL
+	, ProfileID INT REFERENCES Profiles (ProfileID) NOT NULL
+	, Active BIT NOT NULL DEFAULT 1); -- 1 means active in sanctuary
+
+CREATE TABLE CST324_Lake_Robert.GameCategories (
+	GameCategoryID INT PRIMARY KEY IDENTITY
+	, GameCategoryName VARCHAR(50) NOT NULL);
+
+CREATE TABLE CST324_Lake_Robert.MiniGames (
+	MiniGameID INT PRIMARY KEY IDENTITY
+	, MiniGameCategoryID INT REFERENCES GameCategories (GameCategoryID) NOT NULL
+	, MiniGameCode VARCHAR(max) NOT NULL --syntax for clob
+	, MiniGameName VARCHAR(50) NOT NULL
+	, MinDifficulty INT NOT NULL
+	, MaxDifficulty INT NOT NULL);
+
+CREATE TABLE CST324_Lake_Robert.MiniGameMedia (
+	MediaID INT PRIMARY KEY IDENTITY
+	, MiniGameID INT REFERENCES Minigames (MiniGameID) NOT NULL
+	, Difficulty INT DEFAULT NULL	--nulled if media is used for every instance of that game
+	, MiniGameMedia VARBINARY(max) NOT NULL);
+
+CREATE TABLE CST324_Lake_Robert.ProfileProgressHistory (
+	ProgressID INT PRIMARY KEY IDENTITY
+	, ProfileID INT REFERENCES Profiles (ProfileID) NOT NULL
+	, MiniGameID INT REFERENCES Minigames (MiniGameID) NOT NULL);
---
+++ Data Population/ToTheRescueDataPop/ToTheRescueDataPop/ProductDB.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ToTheRescueDataPop
+{
+    class ProductDB
+    {
+        // The directory for the images
+        public static string IMAGES_PATH = "..\\..\\testMedia\\";
+        public static string SOUND_PATH = "..\\..\\testMedia\\";
+        static string DB_USER_NAME = "";
+        static string DB_USER_PWD = "";
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString =
+                "Data Source=aura.students.cset.oit.edu" +
+                ";Initial Catalog=" + DB_USER_NAME +
+                ";Integrated Security=False" +
+                ";User ID=" + DB_USER_NAME + ";Password=" + DB_USER_PWD;
+            return connection;
+        }
+        public static void WriteImage(int ImageClass, string ImagePath, string ImageName) 
+        {
+            SqlConnection connection = null;
+            try
+            {
+                // 1. Read image from file
+                string filepath = IMAGES_PATH + ImagePath;
+                if (File.Exists(filepath) == false)
+                    throw new Exception("File Not Found: " + filepath);
+                FileStream sourceStream = new FileStream(
+                    filepath,
+                    FileMode.Open,
+                    FileAccess.Read);
+
+                int streamLength = (int) sourceStream.Length;
+                Byte[] productImage = new Byte[streamLength];
+                sourceStream.Read(productImage, 0, streamLength);
+                sourceStream.Close();
+
+                // 2. Write image to database
+                connection = GetConnection();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText =
+                    "INSERT INTO dbo.Images (ImageClass, ImageName, Images) " +
+                    "VALUES (@ImageClass, @ImageName, @ProductImage)";
+
+                command.Parameters.AddWithValue("@ImageClass", ImageClass);
+                command.Parameters.AddWithValue("@ImageName", ImageName);
+                command.Parameters.AddWithValue("@ProductImage", productImage);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+        public static void WriteMiniGameMedia(int GameID, string MediaName, int Difficulty)
+        {
+            SqlConnection connection = null;
+            try
+            {
+                // 1. Read image from file
+                string filepath = IMAGES_PATH + MediaName;
+                if (File.Exists(filepath) == false)
+                    throw new Exception("File Not Found: " + filepath);
+                FileStream sourceStream = new FileStream(
+                    filepath,
+                    FileMode.Open,
+                    FileAccess.Read);
+
+                int streamLength = (int)sourceStream.Length;
+                Byte[] productMedia = new Byte[streamLength];
+                sourceStream.Read(productMedia, 0, streamLength);
+                sourceStream.Close();
+
+                // 2. Write image to database
+                connection = GetConnection();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+
+                if (Difficulty.Equals(0))
+                {
+                    command.CommandText =
+                    "INSERT INTO dbo.MiniGameMedia (MiniGameID, MiniGameMedia) " +
+                    "VALUES (@GameID, @ProductMedia)";
+
+                    command.Parameters.AddWithValue("@GameID", GameID);
+                    command.Parameters.AddWithValue("@ProductMedia", productMedia);
+                }
+                else
+                {
+                    command.CommandText =
+                        "INSERT INTO dbo.MiniGameMedia (MiniGameID, Difficulty, MiniGameMedia) " +
+                        "VALUES (@GameID, @Difficulty, @ProductMedia)";
+
+                    command.Parameters.AddWithValue("@GameID", GameID);
+                    command.Parameters.AddWithValue("@ProductMedia", productMedia);
+                    command.Parameters.AddWithValue("@Difficulty", Difficulty);
+                }
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+        public static void WriteSound(int SoundClass, string SoundPath, string SoundName)
+        {
+            SqlConnection connection = null;
+            try
+            {
+                // 1. Read image from file
+                string filepath = SOUND_PATH + SoundPath;
+                if (File.Exists(filepath) == false)
+                    throw new Exception("File Not Found: " + filepath);
+                FileStream sourceStream = new FileStream(
+                    filepath,
+                    FileMode.Open,
+                    FileAccess.Read);
+
+                int streamLength = (int)sourceStream.Length;
+                Byte[] soundImage = new Byte[streamLength];
+                sourceStream.Read(soundImage, 0, streamLength);
+                sourceStream.Close();
+
+                // 2. Write image to database
+                connection = GetConnection();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText =
+                    "INSERT INTO dbo.Sounds (SoundClass, SoundName, Sound) " +
+                    "VALUES (@SoundClass, @SoundName, @SoundImage)";
+                command.Parameters.AddWithValue("@SoundClass", SoundClass);
+                command.Parameters.AddWithValue("@SoundName", SoundName);
+                command.Parameters.AddWithValue("@SoundImage", soundImage);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+        public static List<int> GetImageIDList()
+        {
+            SqlConnection connection = null;
+            try
+            {
+                connection = GetConnection();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText = 
+                    "SELECT ImageID FROM Images " +
+                    "ORDER BY ImageID"; 
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<int> imageIDList = new List<int>();
+                while (reader.Read())
+                {
+                    int imageID = (int) reader[0];
+                    imageIDList.Add(imageID);
+                }
+                reader.Close();
+
+                return imageIDList;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+        public static List<int> GetDataImageIDList()
+        {
+            SqlConnection connection = null;
+            try
+            {
+                connection = GetConnection();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText =
+                   "SELECT MediaID FROM MiniGameMedia " +
+                   "ORDER BY MediaID";
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<int> dataImageIDList = new List<int>();
+                while (reader.Read())
+                {
+                    int imageID = (int)reader[0];
+                    dataImageIDList.Add(imageID);
+                }
+                reader.Close();
+
+                return dataImageIDList;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+        public static List<int> GetSoundIDList()
+        {
+            SqlConnection connection = null;
+            try
+            {
+                connection = GetConnection();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText =
+                    "SELECT SoundID FROM Sounds " +
+                    "ORDER BY SoundID";
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<int> soundIDList = new List<int>();
+                while (reader.Read())
+                {
+                    int soundID = (int)reader[0];
+                    soundIDList.Add(soundID);
+                }
+                reader.Close();
+
+                return soundIDList;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
