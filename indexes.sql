03d5687220c28cca470b99f5dc792f804546f273
Lake Sain-Thomason <lakesainthomason@gmail.com>
2016-12-01 20:06:02 -0800

    fixed index

--- indexes.sql
+++ indexes.sql
@@ -1,6 +1,6 @@
 --Indexes
 create index PPH_Index
-on CST324_Lake_Robert.ProfileProgressHistory (ProgressID)
+on CST324_Lake_Robert.ProfileProgressHistory (ProfileID)
 
 create index UserId_Index
 on CST324_Lake_Robert.Profiles (UserID)
